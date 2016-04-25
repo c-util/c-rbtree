1109dbf12941b12cc365c81a389f6aad237c4cbe
Kay Sievers <kay@vrfy.org>
2016-04-25 15:47:12 +0200

    rename crbtree to c-rbtree, move header file to include root directory

--- src/c-rbtree.h
+++ src/c-rbtree.h
@@ -1,20 +1,20 @@
 #pragma once
 
 /***
-  This file is part of bus1. See COPYING for details.
+  This file is part of c-rbtree. See COPYING for details.
 
-  bus1 is free software; you can redistribute it and/or modify it
+  c-rbtree is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation; either version 2.1 of the License, or
   (at your option) any later version.
 
-  bus1 is distributed in the hope that it will be useful, but
+  c-rbtree is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
   Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public License
-  along with bus1; If not, see <http://www.gnu.org/licenses/>.
+  along with c-rbtree; If not, see <http://www.gnu.org/licenses/>.
 ***/
 
 /*
