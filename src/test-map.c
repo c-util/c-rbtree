919e80b1827ab816ce662fe5209a8c40ba9858c7
David Herrmann <dh.herrmann@gmail.com>
2017-05-13 17:08:00 +0200

    test: add explicit loop tests
    Add tests for the tree iterators, verifying that they actually do what
    they're supposed to do.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/test-map.c
+++ src/test-map.c
@@ -42,9 +42,9 @@ static void shuffle(Node **nodes, size_t n_memb) {
 }
 
 static void test_map(void) {
-        CRBNode **slot, *p;
+        CRBNode **slot, *p, *safe_p;
         CRBTree t = {};
-        Node *nodes[2048];
+        Node *n, *safe_n, *nodes[2048];
         unsigned long i, v;
 
         /* allocate and initialize all nodes */
@@ -126,6 +126,60 @@ static void test_map(void) {
         }
         assert(i == sizeof(nodes) / sizeof(*nodes));
 
+        /* verify in-order traversal works via helper */
+        i = 0;
+        v = 0;
+        c_rbtree_for_each(p, &t) {
+                ++i;
+                assert(!node_from_rb(p)->marker);
+                node_from_rb(p)->marker = 1;
+
+                assert(v <= node_from_rb(p)->key);
+                v = node_from_rb(p)->key;
+
+                assert(!c_rbnode_next(p) || p == c_rbnode_prev(c_rbnode_next(p)));
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+
+        /* verify in-order traversal works via entry-helper */
+        i = 0;
+        v = 0;
+        c_rbtree_for_each_entry(n, &t, rb) {
+                ++i;
+                assert(n->marker);
+                n->marker = 0;
+
+                assert(v <= n->key);
+                v = n->key;
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+
+        /* verify post-order traversal works via helper */
+        i = 0;
+        c_rbtree_for_each_postorder(p, &t) {
+                ++i;
+                assert(!node_from_rb(p)->marker);
+                assert(!c_rbnode_parent(p) || !node_from_rb(c_rbnode_parent(p))->marker);
+                assert(!p->left || node_from_rb(p->left)->marker);
+                assert(!p->right || node_from_rb(p->right)->marker);
+                node_from_rb(p)->marker = 1;
+
+                assert(!c_rbnode_next_postorder(p) || p == c_rbnode_prev_postorder(c_rbnode_next_postorder(p)));
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+
+        /* verify post-order traversal works via entry-helper */
+        i = 0;
+        c_rbtree_for_each_entry_postorder(n, &t, rb) {
+                ++i;
+                assert(n->marker);
+                assert(!c_rbnode_parent(&n->rb) || node_from_rb(c_rbnode_parent(&n->rb))->marker);
+                assert(!n->rb.left || !node_from_rb(n->rb.left)->marker);
+                assert(!n->rb.right || !node_from_rb(n->rb.right)->marker);
+                n->marker = 0;
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+
         /* shuffle nodes again */
         shuffle(nodes, sizeof(nodes) / sizeof(*nodes));
 
@@ -139,12 +193,79 @@ static void test_map(void) {
                 assert(!c_rbnode_is_linked(&nodes[i]->rb));
                 assert(!c_rbtree_find_entry(&t, test_compare, (void *)nodes[i]->key, Node, rb));
         }
+        assert(c_rbtree_is_empty(&t));
+
+        /* add all nodes again */
+        for (i = 0; i < sizeof(nodes) / sizeof(*nodes); ++i) {
+                slot = c_rbtree_find_slot(&t, test_compare, (void *)nodes[i]->key, &p);
+                assert(slot);
+                c_rbtree_add(&t, p, slot, &nodes[i]->rb);
+        }
+
+        /* remove all nodes via helper */
+        i = 0;
+        c_rbtree_for_each_safe(p, safe_p, &t) {
+                ++i;
+                c_rbtree_remove_init(&t, p);
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+        assert(c_rbtree_is_empty(&t));
+
+        /* add all nodes again */
+        for (i = 0; i < sizeof(nodes) / sizeof(*nodes); ++i) {
+                slot = c_rbtree_find_slot(&t, test_compare, (void *)nodes[i]->key, &p);
+                assert(slot);
+                c_rbtree_add(&t, p, slot, &nodes[i]->rb);
+        }
+
+        /* remove all nodes via entry-helper */
+        i = 0;
+        c_rbtree_for_each_entry_safe(n, safe_n, &t, rb) {
+                ++i;
+                c_rbtree_remove_init(&t, &n->rb);
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+        assert(c_rbtree_is_empty(&t));
+
+        /* add all nodes again */
+        for (i = 0; i < sizeof(nodes) / sizeof(*nodes); ++i) {
+                slot = c_rbtree_find_slot(&t, test_compare, (void *)nodes[i]->key, &p);
+                assert(slot);
+                c_rbtree_add(&t, p, slot, &nodes[i]->rb);
+        }
+
+        /* remove all nodes via unlink-helper */
+        i = 0;
+        c_rbtree_for_each_unlink(p, safe_p, &t) {
+                ++i;
+                assert(!c_rbnode_is_linked(p));
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+        assert(c_rbtree_is_empty(&t));
+
+        /* add all nodes again */
+        for (i = 0; i < sizeof(nodes) / sizeof(*nodes); ++i) {
+                slot = c_rbtree_find_slot(&t, test_compare, (void *)nodes[i]->key, &p);
+                assert(slot);
+                c_rbtree_add(&t, p, slot, &nodes[i]->rb);
+        }
+
+        /* remove all nodes via entry-unlink-helper */
+        i = 0;
+        c_rbtree_for_each_entry_unlink(n, safe_n, &t, rb) {
+                ++i;
+                assert(!c_rbnode_is_linked(&n->rb));
+        }
+        assert(i == sizeof(nodes) / sizeof(*nodes));
+        assert(c_rbtree_is_empty(&t));
 
         /* free nodes again */
         for (i = 0; i < sizeof(nodes) / sizeof(*nodes); ++i) {
                 assert(!nodes[i]->marker);
                 free(nodes[i]);
         }
+
+        assert(c_rbtree_is_empty(&t));
 }
 
 int main(int argc, char **argv) {
